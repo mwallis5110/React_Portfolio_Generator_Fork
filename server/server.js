19ded1acbaf06c0e41da7256b4954760ab5c4fb6
Mason Wallis <mwallis5110@gmail.com>
2022-04-23 15:25:24 -0500

    Updated dependencies, fixed index.html minified error

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/public')));
 }
 
 app.post('/api/portfolio', (req, res) => {
@@ -37,7 +37,7 @@ app.get('/api/publicPortfolio/:email', (req, res) => {
 })
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/public/index.html'));
 });
 
 db.once('open', () => {
